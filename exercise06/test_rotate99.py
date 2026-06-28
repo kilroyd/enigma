0e5f8336b15be008711c8bf3d9fcb9bd62912d24
David Kilroy <kilroyd@googlemail.com>
2026-06-28 14:35:41 +0100

    test_rotate: alpha_rotate
    Update the test to use the new function name.

--- exercise06/test_rotate99.py
+++ exercise06/test_rotate99.py
@@ -4,26 +4,26 @@ import sys
 # Tweak lookup path to find students code
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src'))
 
-from enigma import rotate
+from enigma import alpha_rotate
 
 def test_rotate_fwd_nowrap():
     for i in range(26):
-        c = rotate('A', i)
+        c = alpha_rotate('A', i)
         assert c == chr(ord('A') + i)
 
 def test_rotate_rev_nowrap():
     for i in range(26):
-        c = rotate('Z', -i)
+        c = alpha_rotate('Z', -i)
         assert c == chr(ord('Z') - i)
 
 def test_rotate_fwd_wrap():
-    c = rotate('Z', 1)
+    c = alpha_rotate('Z', 1)
     assert c == 'A'
-    c = rotate('Y', 3)
+    c = alpha_rotate('Y', 3)
     assert c == 'B'
 
 def test_rotate_rev_wrap():
-    c = rotate('A', -1)
+    c = alpha_rotate('A', -1)
     assert c == 'Z'
-    c = rotate('B', -3)
+    c = alpha_rotate('B', -3)
     assert c == 'Y'
